98fccc68fdabc922118c471a30fa566a2733906e
Christine Lopez <mrsChristinemlopez@gmail.com>
2025-03-03 22:03:24 -0500

    added the questionaire route in antisipation of the questionnaire api

---
+++ server/src/routes/api/questionnaireRoutes.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { Questionnaire } from '../../../../database/models/questionaire';
+
+const router = express.Router();
+
+//retrieve api questionnaire
+
+router.get('/:id', async (req, res) => {
+try{
+    const questionnaires = await Questionnaire.findAll();
+    res.status (200).json(questionnaires);
+} catch (error) {
+    res.status (500).json({error:error.message});
+
+ }
+});
+
+export default router;
