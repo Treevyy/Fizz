de6b3da39a85a1051041502c53816d73105e4b4e
Christine Lopez <mrsChristinemlopez@gmail.com>
2025-03-03 20:57:20 -0500

    updated the user.js

--- database/models/index.ts
+++ database/models/index.ts
@@ -1,8 +1,15 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-import userModel from './user';
-
+import { User } from './user'
 dotenv.config();
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+//const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+//const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
 const sequelize = new Sequelize(
   process.env.DB_NAME as string,
--- models/user.js
+++ models/user.js
@@ -1,25 +1,26 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+// filepath: /c:/Users/mrsch/Fizz/models/user.js
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   });
+
+  User.associate = (models) => {
+    // Define associations here
+  };
+
   return User;
 };
+
