b649a1da6bf57e64e44d3c31ce116c473d1a45cd
Christine L <187074331+nita9801@users.noreply.github.com>
2025-03-04 19:27:37 -0500

    updated the model name for import

---
+++ database/models/userModel.ts
@@ -0,0 +1,40 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import { sequelize } from './database'; 
+
+const userModel = sequelize.define ('User',
+  {
+    
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {  
+    tableName: 'usersData',  
+});
+export { userModel };
