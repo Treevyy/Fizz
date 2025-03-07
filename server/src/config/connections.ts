452ac8fe15a480bb64a79357379537f9a722a36e
Jacquilyn F <jacquilyn.fletcher89@gmail.com>
2025-03-06 18:48:32 -0600

    fixed config and combined files

---
+++ server/src/config/connections.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql', // or the dialect you are using, e.g., 'postgres', 'sqlite', 'mssql'
+});
+
+export default sequelize;
